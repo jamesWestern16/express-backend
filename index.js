f54eeb4ab7b7c119d3f636eb50de58734934f744
James Western <jameswestern16@gmail.com>
2022-09-26 22:17:47 +0100

    make examples of various core backend pronciples

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+app.use(express.json()) 
+const port = 3000
+const topScores = [397]
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/bye', (req, res) => {
+    res.send('Goodbye World!')
+  })
+  
+app.get('/scores', (req, res) => {
+    res.send(topScores)
+  })
+
+app.post('/scores', (req, res) => {
+    console.log(req.body.scores);
+    topScores.push (req.body.scores)
+    res.send('Goodbye World!')
+  })
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// console.log(typeof express);
+// console.log(typeof app);
+// function cat(name) {
+//     return "your cat's name is" +name
+// }
+
+// console.log ("What is the name?")
+// const result = cat("Billy")
+
+// console.log(result);
